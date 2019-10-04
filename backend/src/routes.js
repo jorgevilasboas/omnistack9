d8b84c1de55e9d810dbfd3725a8b4025bed36162
jorgevilasboas <jorge.sergipe@gmail.com>
2019-10-04 17:37:15 -0300

    Adds comments

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,9 +3,9 @@ const express = require('express');
 const routes = express.Router();
 
 routes.get('/users/:id', (req, res) => {
-    //by query
+    //by query /users/1/?idade=30
     let idade = req.query.idade;
-    //by params
+    //by params /users/1/
     let id = req.params.id;
     return res.json({ message: "Hello", idade, id });
 
