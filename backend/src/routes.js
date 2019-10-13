aa5e26b4c78c43ecc890f63332fe3e4058c0d280
jorgevilasboas <jorge.sergipe@gmail.com>
2019-10-13 15:56:38 -0300

    finish app backend

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,7 +1,26 @@
 const express = require('express');
+const multer = require('multer');
+const uploadConfig = require('./config/upload');
+
+const SessionController = require('./controllers/SessionController');
+const SpotController = require('./controllers/SpotController');
+const DashboardController = require('./controllers/DashboardController');
+const BookingController = require('./controllers/BookingController');
+
 
 const routes = express.Router();
+const upload = multer(uploadConfig);
 
+routes.post('/sessions', SessionController.store);
+
+routes.get('/spots', SpotController.index);
+routes.post('/spots', upload.single('thumbnail'),  SpotController.store);
+
+routes.get('/dashboard', DashboardController.show);
+
+routes.post('/spots/:spot_id/bookings', BookingController.store);
+
+/*
 routes.get('/users/:id', (req, res) => {
     //by query /users/1/?idade=30
     let idade = req.query.idade;
@@ -15,5 +34,6 @@ routes.post('/users', (req, res) => {
     //by body    
     return res.json(req.body);
 });
+*/
 
 module.exports = routes;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes');
 
+const password = 'cCpFV8rx3jFfHux';
+const databasename = 'omni9'
 const app = express();
+
+    mongoose.connect(`mongodb://jorge:${password}@omni9-shard-00-00-8cn0q.mongodb.net:27017,omni9-shard-00-01-8cn0q.mongodb.net:27017,omni9-shard-00-02-8cn0q.mongodb.net:27017/${databasename}?ssl=true&replicaSet=Omni9-shard-0&authSource=admin&retryWrites=true&w=majority`,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })    
+
 
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+app.listen(3333, () => {console.log('Listening on port 3333')});
